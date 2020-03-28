a4639925fdb88890899c909723ddcb0dc8a145d2
TastX <tastx128@gmail.com>
2020-03-28 20:10:59 +0100

    a et b transformés en i et j

--- prefixe_suffixe.c
+++ prefixe_suffixe.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 
 prefixe_suffixe(int *t, int N){
+	assert(t != NULL && N > 0);
 
-	int a = 0, b = 1, ctr = 0;
+	int i = 0, j = 1, ctr = 0;
 
-	while(b < N){
+	while(j < N){
 
-		if(t[a] == t[b] && t[N-1-b] == t[N-1-a]){
+		if(t[i] == t[j] && t[N-1-j] == t[N-1-i]){
 			ctr++;
-			a++;
+			i++;
 		}
 
 		else{
 			ctr = 0;
 		}
 
-		b++;
+		j++;
 	}
+	return ctr;
 }
