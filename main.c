33bc5fdc1a36e9d49a287f5d7c68cdffdf49eb31
TastX <tastx128@gmail.com>
2020-03-28 13:21:36 +0100

    transformé c en N-1-b

--- main.c
+++ main.c
@@ -2,12 +2,11 @@
 #include <stdlib.h>
 
 int main(){
-	const int N = 5;
+	const int N = 10;
 	int a = 0, b = 1, c = N-2, d = N-1, ctr = 0;
-	int t[5] = {2, 1, 2, 1, 2};
+	int t[10] = {1, 2, 1, 3, 1, 2, 1, 2, 1, 3};
 	while(b < N){
-		//printf("a = %d, b = %d, c = %d, d = %d\n t[a] = %d, t[b] = %d, t[c] = %d, t[d] = %d\n\n", a, b, c, d, t[a], t[b], t[c], t[d]);
-		if(t[a] == t[b] && t[c] == t[d]){
+		if(t[a] == t[b] && t[N-1-b] == t[d]){
 			ctr++;
 			a++;
 			d--;
@@ -16,7 +15,6 @@ int main(){
 			ctr = 0;
 		}
 		b++;
-		c--;
 	}
 	printf("%d\n", ctr);
 }
