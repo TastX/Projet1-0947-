034485f346572fee2b5732d225518a8748addf85
TastX <tastx128@gmail.com>
2020-03-28 20:28:28 +0100

    bon dossier

--- code/prefixe_suffixe.c
+++ code/prefixe_suffixe.c
@@ -1,6 +1,17 @@
+/**
+ * prefixe_suffixe.c
+ *
+ * Ce fichier contient la focntion prefixe_suffixe
+ *
+ * @authors: Seel Antoine 20180459 &&  Iglesias Raphaël
+ * @date: 28/03/20
+ * @projet: INFO0947 Projet 1
+ */
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+
+#include "prefixe_suffixe.h"
 
 prefixe_suffixe(int *t, int N){
 	assert(t != NULL && N > 0);
---
+++ code/prefixe_suffixe.h
@@ -0,0 +1,10 @@
+/**
+ * prefixe_suffixe.h
+ *
+ * Ce fichier contient le prototype de la fonction
+ * prefixe_suffixe
+ *
+ * @authors: Seel Antoine 20180459 &&  Iglesias Raphaël
+ * @date: 28/03/20
+ * @projet: INFO0947 Projet 1
+ */
